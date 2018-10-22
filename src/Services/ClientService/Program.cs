e4764086ae56577a7b8b6a04f27b6270dbc4e13a
Hans Erik Heggem <heh@dips.no>
2018-10-22 11:30:01 +0200

    added random selection of possible API services to call from client

--- src/Services/ClientService/Program.cs
+++ src/Services/ClientService/Program.cs
@@ -21,37 +21,47 @@ namespace ClientService
             var tags = new[] {"ApiService", "consulTesting", "ILuuveCaaS"};
             while (true)
             {
-                Thread.Sleep(TimeSpan.FromSeconds(3));
+                Thread.Sleep(TimeSpan.FromSeconds(5));
                 ClientTask(tags).Wait();
             }
         }
 
         private static async Task ClientTask(string[] tags)
         {
-            var serverUrls = await GetServerUrls(tags);
-            await PollServerUrls(serverUrls);
+            var serverUris = await GetServerUris(tags);
+            var serverUri = SelectRandomUri(serverUris);
+            await PollServerUris(serverUri);
         }
 
-        private static async Task PollServerUrls(Uri[] uris)
+        private static async Task PollServerUris(Uri uri)
         {
             var client = new HttpClient();
-            foreach (var uri in uris)
+            var requestUri = new Uri(uri, "api/values");
+            Console.WriteLine($"Polling api service: {requestUri}");
+            var response = await client.GetAsync(requestUri);
+            if (!response.IsSuccessStatusCode)
             {
-                var requestUri = new Uri(uri, "api/values");
-                Console.WriteLine($"Polling api service: {requestUri}");
-                var response = await client.GetAsync(requestUri);
-                if (!response.IsSuccessStatusCode)
-                {
-                    throw new Exception($"Could not get api values from: {requestUri}");
-                }
-                var content = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Got api values from: {requestUri} with content: {content}");
+                throw new Exception($"Could not get api values from: {requestUri}");
             }
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Got api values from: {requestUri} with content: {content}");
         }
 
-        private static async Task<Uri[]> GetServerUrls(string[] tags)
+        private static Uri SelectRandomUri(Uri[] uris)
         {
-            var serverUrls = new List<Uri>();
+            Console.WriteLine("Selecting a random uri from: ");
+            foreach (var uri in uris)
+            {
+                Console.WriteLine($"\t - {uri}");
+            }
+            var random = new Random();
+            var randomNumber = random.Next(0, uris.Length);
+            return uris[randomNumber];
+        }
+
+        private static async Task<Uri[]> GetServerUris(string[] tags)
+        {
+            var serverUris = new List<Uri>();
             var consulClient = new ConsulClient(c => c.Address = new Uri($"http://{ConsulHost}:8500"));
             var services = (await consulClient.Agent.Services()).Response;
             foreach (var service in services)
@@ -59,13 +69,13 @@ namespace ClientService
                 var isApi = service.Value.Tags.All(t => tags.Any(tag => t == tag));
                 if (!isApi)
                 {
-                    throw new Exception($"No tags matched any registered services on Consul");
+                    throw new Exception($"The tags did not match any registered services on Consul");
                 }
                 var serviceUri = new Uri($"http://{service.Value.Address}:{service.Value.Port}");
-                serverUrls.Add(serviceUri);
+                serverUris.Add(serviceUri);
             }
 
-            return serverUrls.ToArray();
+            return serverUris.ToArray();
         }
     }
 }
